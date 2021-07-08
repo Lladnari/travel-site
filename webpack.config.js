c037e2fc57d8b058a91ad5175f4ee3999d66f2f0
Randall Ijams <Randall@Ijams.net>
2021-07-08 15:52:28 -0700

    Completed large hero and intro content blocks.

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        before: function (app, server){ // auto update browser URL of localhost:<<port>> upon changes to *.html files.
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true, // inject css and js.
+        port: 3000, // 8080 by default.
+        host: '0.0.0.0' // allow devices on same network to access WebPack-Dev-Server from this computer.
+    },
+    mode: 'development',
+    watch: false, // not needed due to devServer.
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader', 
+                    'css-loader?url=false', // css-loader attempts to handle any images. Disable this so we can manage images manually.
+                    {
+                        loader: 'postcss-loader', 
+                        options: {
+                            postcssOptions: {
+                                plugins: postCSSPlugins
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
