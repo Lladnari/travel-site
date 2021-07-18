7e08ad671e42e67b6a201237a614ccd43f3bc874
Randall Ijams <Randall@Ijams.net>
2021-07-18 14:00:21 -0700

    Finished modal lesson.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,17 +1,40 @@
 // App.js
 
-let iDebug = 5;
+let iDebug = 0;
 
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
 import StickyHeader from './modules/StickyHeader.js';
 
+// import Modal from './modules/Modal.js';
+// new Modal();
+
 let stickyHeader = new StickyHeader();
 let oMobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 85);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 75);
+let modal; /* Create undefined global variable to use in functions. */
+
+document.querySelectorAll(".open-modal").forEach(elm => {
+    elm.addEventListener("click", evnt => {
+        let iDelayMS = 100;
+        let sFileName = "./modules/Modal.js";
+        evnt.preventDefault();
+        if (typeof modal == "undefined") { // first request.
+            if (iDebug > 8) { console.log("Loading Modal.js..."); }
+            import(/* webpackChunkName: "modal" */ "./modules/Modal.js").then(mFile => {
+                setTimeout( () => modal = new mFile.default(), 10);
+                setTimeout( () => modal.openTheModal(), iDelayMS); // wait until object is ready before calling.
+                if (iDebug > 8) { console.log(sFileName + " loaded after "+ iDelayMS + "MS."); }
+            }).catch( () => console.log(sFileName + " was not loaded.") );
+        } else {
+            modal.openTheModal();
+            if (iDebug > 8) { console.log("Modal opened."); }
+        }
+    }) // elm.addEventListener()
+}) // ...forEach()
 
 if (module.hot) {
-    module.hot.accept();
+    module.hot.accept(); // Whjy?
 }
--- app/assets/scripts/modules/Variables.js
+++ app/assets/scripts/modules/Variables.js
@@ -1,3 +1,4 @@
 // Variables.js
 
+// 1-9, 1=borders, 2=, 5=functions, 9=variable values.
 let iDebug = 5;
